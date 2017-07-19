b6b3f4362355850ff04cc45a26f2adc8b14f3176
Bill Newman <william.newman@seattlecolleges.edu>
2017-07-19 14:19:24 -0700

    Adds first version of survey_view.php This version does not yet utilize classes.

---
+++ surveys/survey_view.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * index.php along with survey_view.php provides a sample web application
+ *
+ * The difference between demo_list.php and index.php is the reference to the 
+ * Pager class which processes a mysqli SQL statement and spans records across multiple  
+ * pages. 
+ *
+ * The associated view page, survey_view.php is virtually identical to demo_view.php. 
+ * The only difference is the pager version links to the list pager version to create a 
+ * separate application from the original list/view. 
+ *  
+ * @package SurveySez
+ * @author Joe Blow <joe.blow@example.com>
+ * @version 0.1 2017/07/19
+ * @link http://www.example.com/porfolio/
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see index.php
+ * @todo none
+ */
+
+# '../' works for a sub-folder.  use './' for the root  
+require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
+ 
+# check variable of item passed in - if invalid data, forcibly redirect back to index.php page
+if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystring
+	 $myID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
+}else{
+	myRedirect(VIRTUAL_PATH . "surveys/index.php");
+}
+
+//sql statement to select individual item
+//$sql = "select MuffinName,Description,MetaDescription,MetaKeywords,Price from test_Muffins where MuffinID = " . $myID;
+$sql = "select Title,Description from sm17_surveys where SurveyID = " . $myID;
+//---end config area --------------------------------------------------
+
+$foundRecord = FALSE; # Will change to true, if record found!
+   
+# connection comes first in mysqli (improved) function
+$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+if(mysqli_num_rows($result) > 0)
+{#records exist - process
+	   $foundRecord = TRUE;	
+	   while ($row = mysqli_fetch_assoc($result))
+	   {
+			$Title = dbOut($row['Title']);
+			$Description = dbOut($row['Description']);
+	   }
+}
+
+@mysqli_free_result($result); # We're done with the data!
+
+if($foundRecord)
+{#only load data if record found
+	$config->titleTag = $Title . " surveys made with PHP & love!"; #overwrite PageTitle with Muffin info!
+}
+
+# END CONFIG AREA ---------------------------------------------------------- 
+get_header(); #defaults to theme header or header_inc.php
+if($foundRecord)
+{#records exist - show survey!
+ echo '
+ <h3 align="center">' . $Title . '</h3>
+    <p>' . $Description . '</p>
+ ';
+
+}else{//no such survey!
+    echo '<div align="center">What! No such survey? There must be a mistake!!</div>';
+}
+
+echo '<div align="center"><a href="' . VIRTUAL_PATH . 'surveys/index.php">Back</a></div>';
+
+get_footer(); #defaults to theme footer or footer_inc.php
+?>
